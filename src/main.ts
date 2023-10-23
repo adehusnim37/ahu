9fc9fa557427cf104b1f0101ef472381da0d07e8
adehusnim37 <adehusni37@gmail.com>
2023-10-23 20:16:14 +0700

    0.1.3 : enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        allowedHeaders: 'Content-Type, Accept',
+    })
+    await app.listen(4000);
 }
+
 bootstrap();
